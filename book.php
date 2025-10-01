77f32720d767981e8e1a105b62befc0b2d45857a
Alwina Mohammad <zryukai@gmail.com>
2025-10-02 07:24:31 +0800

    Update book.php

--- book.php
+++ book.php
@@ -22,7 +22,7 @@ class Book extends Database {
         $query->bindParam(":publication_year", $this->publication_year);
         $query->bindParam(":publisher", $this->publisher);
         $query->bindParam(":copies", $this->copies);
-
+        $query->bindParam(":id", $id);
         return $query->execute();
     }
 
@@ -57,4 +57,36 @@ class Book extends Database {
 
         return ($record["total"] > 0);
     }
+    public function fetchBook($id) {
+    $sql = "SELECT * FROM book WHERE id = :id LIMIT 1";
+    $query = $this->connect()->prepare($sql);
+    $query->bindParam(":id", $id);
+    $query->execute();
+    return $query->fetch();
+    }
+
+    public function deleteBook($id) {
+    $sql = "DELETE FROM book WHERE id = :id";
+    $query = $this->connect()->prepare($sql);
+    $query->bindParam(":id", $id);
+    return $query->execute();
+    }
+    public function updateBook($id) {
+        $sql = "UPDATE book 
+                SET title = :title, author = :author, genre = :genre, 
+                    publication_year = :publication_year, publisher = :publisher, copies = :copies
+                WHERE id = :id";
+        $query = $this->connect()->prepare($sql);
+
+        $query->bindParam(":title", $this->title);
+        $query->bindParam(":author", $this->author);
+        $query->bindParam(":genre", $this->genre);
+        $query->bindParam(":publication_year", $this->publication_year);
+        $query->bindParam(":publisher", $this->publisher);
+        $query->bindParam(":copies", $this->copies);
+        $query->bindParam(":id", $id);
+
+        return $query->execute();
+    }
 }
+
