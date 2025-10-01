cedb7921cec69257735b71b3e69f8d9c60ad6a5e
Alwina Mohammad <zryukai@gmail.com>
2025-10-02 07:22:50 +0800

    Update deletebook.php

--- deletebook.php
+++ deletebook.php
@@ -1,1 +1,36 @@
+<?php
 
+require_once "../classes/book.php";
+$bookObj = new Book();
+
+
+$book = [];
+$errors = [];
+$id = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "GET")
+    {
+        if (isset($_GET["id"]))
+        {
+            $id = trim(htmlspecialchars($_GET["id"]));
+            $book = $bookObj->fetchbook($id);
+
+            if (!$book)
+            {
+                echo "<a href='viewbook.php'>View Book</a>";
+                exit("book Not Found");
+            }
+            else
+            {
+                $bookObj->deleteBook($id);
+                header("Location: viewbook.php");
+            }
+        }
+        else
+        {
+            echo "<a href='viewbook.php'>View book</a>";
+            exit("book Not Found");
+        }
+    }
+
+?>
